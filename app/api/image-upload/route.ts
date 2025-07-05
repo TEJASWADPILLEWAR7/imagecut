b47a0f5f443408dab5de99adedd2b514175535e8
TEJASWADPILLEWAR7 <tejaswadpillewar7@gmail.com>
2025-07-05 18:28:33 +0530

    route error fix

--- app/api/image-upload/route.ts
+++ app/api/image-upload/route.ts
@@ -15,7 +15,7 @@ interface CloudinaryUploadResult {
 }
 
 export async function POST(request: NextRequest) {
-  const { userId } = auth();
+  const { userId } = await auth();
   if (!userId) {
     return NextResponse.json(
       { error: "Please login to upload" },
